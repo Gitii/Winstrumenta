d26de4179b676447bb53b386e35d207a06e037ed
Gitii <Gitii@users.noreply.github.com>
2022-03-06 15:04:21 +0100

    Updated assets. Added DistroManager stub.

--- PackageInstaller.Core.Services.Tests/DebianVersionTests.cs
+++ PackageInstaller.Core.Services.Tests/DebianVersionTests.cs
@@ -8,25 +8,34 @@ public class DebianVersionTests
 {
     private static readonly object?[] VersionParseTestCases =
     {
-        new object?[] { "1:1.4.1-1", 1u, "1.4.1", "1" }, new object?[] { "7.1.ds-1", 0u, "7.1.ds", "1" },
+        new object?[] { "1:1.4.1-1", 1u, "1.4.1", "1" },
+        new object?[] { "7.1.ds-1", 0u, "7.1.ds", "1" },
         new object?[] { "10.11.1.3-2", 0u, "10.11.1.3", "2" },
         new object?[] { "4.0.1.3.dfsg.1-2", 0u, "4.0.1.3.dfsg.1", "2" },
         new object?[] { "0.4.23debian1", 0u, "0.4.23debian1", null },
         new object?[] { "1.2.10+cvs20060429-1", 0u, "1.2.10+cvs20060429", "1" },
         new object?[] { "0.2.0-1+b1", 0u, "0.2.0", "1+b1" },
         new object?[] { "4.3.90.1svn-r21976-1", 0u, "4.3.90.1svn-r21976", "1" },
-        new object?[] { "1.5+E-14", 0u, "1.5+E", "14" }, new object?[] { "20060611-0.0", 0u, "20060611", "0.0" },
-        new object?[] { "0.52.2-5.1", 0u, "0.52.2", "5.1" }, new object?[] { "7.0-035+1", 0u, "7.0", "035+1" },
+        new object?[] { "1.5+E-14", 0u, "1.5+E", "14" },
+        new object?[] { "20060611-0.0", 0u, "20060611", "0.0" },
+        new object?[] { "0.52.2-5.1", 0u, "0.52.2", "5.1" },
+        new object?[] { "7.0-035+1", 0u, "7.0", "035+1" },
         new object?[] { "1.1.0+cvs20060620-1+2.6.15-8", 0u, "1.1.0+cvs20060620-1+2.6.15", "8" },
         new object?[] { "1.1.0+cvs20060620-1+1.0", 0u, "1.1.0+cvs20060620", "1+1.0" },
         new object?[] { "4.2.0a+stable-2sarge1", 0u, "4.2.0a+stable", "2sarge1" },
-        new object?[] { "1.8RC4b", 0u, "1.8RC4b", null }, new object?[] { "0.9~rc1-1", 0u, "0.9~rc1", "1" },
+        new object?[] { "1.8RC4b", 0u, "1.8RC4b", null },
+        new object?[] { "0.9~rc1-1", 0u, "0.9~rc1", "1" },
         new object?[] { "2:1.0.4+svn26-1ubuntu1", 2u, "1.0.4+svn26", "1ubuntu1" },
         new object?[] { "2:1.0.4~rc2-1", 2u, "1.0.4~rc2", "1" },
     };
 
     [TestCaseSource(nameof(VersionParseTestCases))]
-    public void ShouldParseVersion(string rawVersionString, uint epoch, string upstream, string? debian)
+    public void ShouldParseVersion(
+        string rawVersionString,
+        uint epoch,
+        string upstream,
+        string? debian
+    )
     {
         var v = new DebianVersion(rawVersionString);
 
@@ -47,27 +56,41 @@ public class DebianVersionTests
 
     private static readonly object[] VersionComparisonTestSources =
     {
-        new object?[] { "0", "<", "a" }, new object?[] { "1.0", "<", "1.1" }, new object?[] { "1.2", "<", "1.11" },
-        new object?[] { "1.0-0.1", "<", "1.1" }, new object?[] { "1.0-0.1", "<", "1.0-1" },
-        new object?[] { "1.0", "==", "1.0" }, new object?[] { "1.0-0.1", "==", "1.0-0.1" },
-        new object?[] { "1:1.0-0.1", "==", "1:1.0-0.1" }, new object?[] { "1:1.0", "==", "1:1.0" },
-        new object?[] { "1.0-0.1", "<", "1.0-1" }, new object?[] { "1.0final-5sarge1", ">", "1.0final-5" },
+        new object?[] { "0", "<", "a" },
+        new object?[] { "1.0", "<", "1.1" },
+        new object?[] { "1.2", "<", "1.11" },
+        new object?[] { "1.0-0.1", "<", "1.1" },
+        new object?[] { "1.0-0.1", "<", "1.0-1" },
+        new object?[] { "1.0", "==", "1.0" },
+        new object?[] { "1.0-0.1", "==", "1.0-0.1" },
+        new object?[] { "1:1.0-0.1", "==", "1:1.0-0.1" },
+        new object?[] { "1:1.0", "==", "1:1.0" },
+        new object?[] { "1.0-0.1", "<", "1.0-1" },
+        new object?[] { "1.0final-5sarge1", ">", "1.0final-5" },
         new object?[] { "1.0final-5", ">", "1.0a7-2" },
         new object?[] { "0.9.2-5", "<", "0.9.2+cvs.1.0.dev.2004.07.28-1.5" },
-        new object?[] { "1:500", "<", "1:5000" }, new object?[] { "100:500", ">", "11:5000" },
-        new object?[] { "1.0.4-2", ">", "1.0pre7-2" }, new object?[] { "1.5~rc1", "<", "1.5" },
-        new object?[] { "1.5~rc1", "<", "1.5+b1" }, new object?[] { "1.5~rc1", "<", "1.5~rc2" },
+        new object?[] { "1:500", "<", "1:5000" },
+        new object?[] { "100:500", ">", "11:5000" },
+        new object?[] { "1.0.4-2", ">", "1.0pre7-2" },
+        new object?[] { "1.5~rc1", "<", "1.5" },
+        new object?[] { "1.5~rc1", "<", "1.5+b1" },
+        new object?[] { "1.5~rc1", "<", "1.5~rc2" },
         new object?[] { "1.5~rc1", ">", "1.5~dev0" },
     };
 
     [TestCaseSource(nameof(VersionComparisonTestSources))]
-    public void ShouldCompareVersion(string leftRawVersion, string comparisonOperator, string rightRawVersion)
+    public void ShouldCompareVersion(
+        string leftRawVersion,
+        string comparisonOperator,
+        string rightRawVersion
+    )
     {
         var left = new DebianVersion(leftRawVersion);
         var right = new DebianVersion(rightRawVersion);
 
-        GetVersionComparator<DebianVersion>(comparisonOperator)(left, right).Should()
-            .BeTrue($"{leftRawVersion} {comparisonOperator} {right}");
+        GetVersionComparator<DebianVersion>(comparisonOperator)(left, right)
+            .Should()
+            .BeTrue($"{leftRawVersion} {comparisonOperator} {rightRawVersion}");
     }
 
     public static Func<T, T, bool> GetVersionComparator<T>(string comparisonOperator)
