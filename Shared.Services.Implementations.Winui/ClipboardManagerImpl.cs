16c9678c084a3a89dacaf7f784ea98c391c98010
Gitii <Gitii@users.noreply.github.com>
2023-03-09 12:56:48 +0100

    Add "Found a bug" button on error page (#30)

---
+++ Shared.Services.Implementations.Winui/ClipboardManagerImpl.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.DataTransfer;
+
+namespace Shared.Services.Implementations.WinUI;
+
+public class ClipboardManagerImpl : IClipboardManager
+{
+    public Task CopyAsync(string content)
+    {
+        var package = new DataPackage();
+        package.SetText(content);
+
+        Clipboard.SetContent(package);
+        return Task.CompletedTask;
+    }
+}
